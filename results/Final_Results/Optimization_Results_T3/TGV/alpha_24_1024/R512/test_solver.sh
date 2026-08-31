8469c628be9a77a05653b88b9dcf2b489acfe9cb
benslern <benslern@mcmaster.ca>
2026-08-31 10:42:31 -0400

    Finish Opt T3 TGV A24

---
+++ results/Final_Results/Optimization_Results_T3/TGV/alpha_24_1024/R512/test_solver.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=def-bprotas
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=192
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-24:00
+#SBATCH --output=./results/test_solver_%j.out
+#SBATCH --job-name=R256_TGV_A24
+
+cd $SLURM_SUBMIT_DIR
+
+module load StdEnv/2023
+module load fftw-mpi/3.3.10
+module load netcdf-fortran-mpi/4.6.1 
+module load intel/2023.2.1
+
+source /scinet/vast/etc/vastpreload-openmpi.bash
+
+# EXECUTION COMMAND; ampersand off 3 sub-jobs, 64-tasks-each and wait
+(mpirun -N 128 ./test_solver && echo "job 1 finished") &
+wait
+
+#(mpirun -N 64 ./test_solver 2 && echo "job 2 finished") &
+#(mpirun -N 64 ./test_solver 3 && echo "job 3 finished") &
